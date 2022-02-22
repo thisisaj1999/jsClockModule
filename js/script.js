499b0b1a8669a467802ee3c7a50c9ce9a6054d1d
thisisaj1999 <atuljoshi94170@gmail.com>
2022-02-22 21:12:05 +0530

    hours, minutes, seconds functionality added.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const setDate = function () {
+  const now = new Date();
+
+  //   For Seconds
+  const seconds = now.getSeconds();
+  const secondsDeg = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDeg}Deg)`;
+
+  //   For Minutes
+  const minutes = now.getMinutes();
+  const minutesDeg = (minutes / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minutesDeg}Deg)`;
+
+  //   For Hours
+  const hours = now.getHours();
+  const hoursDeg = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDeg}Deg)`;
+};
+
+setInterval(setDate, 1000);
